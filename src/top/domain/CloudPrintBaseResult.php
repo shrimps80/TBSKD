152c74aa153da8dedb6072712ac6e27f0c401621
shrimp <1732341@qq.com>
2017-05-13 17:59:52 +0800

    v20170513

---
+++ src/top/domain/CloudPrintBaseResult.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * result
+ * @author auto create
+ */
+class CloudPrintBaseResult
+{
+	
+	/** 
+	 * data
+	 **/
+	public $data;
+	
+	/** 
+	 * 错误码
+	 **/
+	public $error_code;
+	
+	/** 
+	 * 错误消息
+	 **/
+	public $error_message;
+	
+	/** 
+	 * 状态
+	 **/
+	public $success;	
+}
+?>
